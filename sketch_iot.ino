dbffc743c2368bfae5187a3b8db0d96d668ab59a
Malshani RANCHA GODAGE <43683422+rgmprabodha@users.noreply.github.com>
2020-01-10 19:25:13 +0100

    Add some comments

--- sketch_iot.ino
+++ sketch_iot.ino
@@ -1,5 +1,4 @@
 #include <string.h>
-
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include <Wire.h>
@@ -8,35 +7,33 @@
 #include <DallasTemperature.h>
 #include <MQTT.h>
 
-
-/* We need objects to handle WiFi connectivity
- */
+/* Objects to handle WiFi connectivity */
 WiFiClient wifi_client ;
 MQTTClient mqtt_client ;
 
-byte server[] = { 192, 168, 1, 131 };
-//byte APIserver[]={182, 42, 117, 192};
+/* Server address */
+byte server[] = { 192, 168, 1, 131 }; 
 
-/* In which pins is my buttons plugged? */
-#define POTENTIOMETER_PIN A4 // TODO: uncomment this line if the potentiometer is connected to analogic pin 4, or adapt...
-//#define POTENTIOMETER_PIN 4 // TODO: uncomment this line if the potentiometer is connected to digital pin 3, or adapt...
+/*Potentiometer is connected to Analog pin 4*/
+#define POTENTIOMETER_PIN A4 
 
-// TODO: you probably need to add a line here for the button pin....
 #define BUTTON_PIN 2
 
-/* And associated variables to tell:
- *  1. which WiFi network to connect to
- *  2. what are the MQTT broket IP address and TCP port
- */
-const char* wifi_ssid     = "lab-iot-1"; // TODO: change this
-const char* wifi_password = "lab-iot-1"; // TODO: change this
+/* Wifi info*/
+const char* wifi_ssid     = "lab-iot-1"; 
+const char* wifi_password = "lab-iot-1"; 
+
+/*MQTT info*/
 const char* mqtt_host = "max.isasecret.com" ;
 const uint16_t mqtt_port =  1723 ;
+
 /* Some variables to handle measurements. */
 int potentiometerValue ;
-int buttonState = 0;         // variable for reading the pushbutton status
-int pot0=0;
-String topic = "/building/"  ;
+int buttonState = 0;         
+int pot0 = 0;
+
+/*MQTT topic for switch light*/
+String topic = "/switchLight/-10"  ;
 
 uint32_t t0, t ;
 
@@ -58,13 +55,9 @@ void Wifireconnect() {
   Serial.print("IP address: \n");
   Serial.print(WiFi.localIP());
   Serial.print("\n") ;
-
-
 }
 
-
 void setup() {
-  // monitoring via Serial is always nice when possible
   Serial.begin(9600) ;
   delay(100) ;
   Serial.println("Initializing...\n") ;
@@ -80,29 +73,21 @@ void setup() {
 
   // Time begins now!
   t0 = t = millis() ;
-
-  
-
-  
 }
-
-
 
 void loop() {
    mqtt_client.loop() ;
-  if ( ! mqtt_client.connected() ) {
-//    reconnect() ;
-  }
+     if ( ! mqtt_client.connected() ) {
+   }
    
-  t = millis() ;
-  if ( (t - t0) >= DELTA_T ) {
+   t = millis() ;
+   if ( (t - t0) >= DELTA_T ) {
     Wifireconnect();
     t0 = t ;
     
     // read the states:
     buttonState = digitalRead(BUTTON_PIN);
-    // potentiometerValue = digitalRead(POTENTIOMETER_PIN); // TODO: uncomment this line if it is digital
-    potentiometerValue = analogRead(POTENTIOMETER_PIN); // TODO; uncomment this line if it is analog
+    potentiometerValue = analogRead(POTENTIOMETER_PIN); 
     
     Serial.print("potentiometer value: ");
     Serial.print(potentiometerValue);
@@ -128,14 +113,11 @@ void loop() {
           }
           else{
         Serial.println("not connected/sending");
-         }
-    
-    
-    
+         } 
   }
-  if (abs(pot0-potentiometerValue)>10){
-    if (wifi_client.connect(server, 80)){
-      
+   
+  if (abs(pot0-potentiometerValue) > 10){
+    if (wifi_client.connect(server, 80)){      
         wifi_client.println("PUT /api/0KX7DYkM6LgQ2rDmuF3zr94QY5TrsYLAqWmeIGQN/lights/15/state HTTP/1.1");        
         wifi_client.println("Host: 192.168.1.131");
         wifi_client.println("Connection: keep-alive");
@@ -145,15 +127,15 @@ void loop() {
         wifi_client.print("{\"on\":true,\"sat\":255,\"bri\":");
         wifi_client.print(potentiometerValue);
         wifi_client.println("}");
-        Serial.println("sent"); 
-        
+        Serial.println("sent");         
     }
     else{
         Serial.println("not connected/sending");
      }
-    
   }
   pot0=potentiometerValue;
+   
+   // TODO get current status of light to make the switch flexible
  /* String getLightStatus(){
      if (wifi_client.connect(server, 80)) {
             Serial.println("connected to server");
@@ -164,23 +146,12 @@ void loop() {
             wifi_client.println("Content-Type: application/json");
             wifi_client.println("Content-Length: 51");
             wifi_client.println();
-          //  wifi_client.println("{\"on\":true}");
-                                
+            wifi_client.println("{\"on\":true}");                                
           }
           else{
         Serial.println("not connected/sending");
-         }
-    
-    }*/
-  
-  
-  
- 
+         }    
+    }*/  
+}
 
-    
-    
-    // TODO: adapt this program to connect to the WiFi and send the HTTP request to the Philips Hue here.
-    // WARNING: send at most one HTTP request per second !!! and only if it makes sense, i.e., if the values did not change too much.
-
-  
-}}
+}
